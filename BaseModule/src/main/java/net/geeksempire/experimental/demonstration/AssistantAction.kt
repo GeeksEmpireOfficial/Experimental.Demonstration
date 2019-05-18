2715e3e823c10d9a88e637b2e2b4c4c3dd92ad2d
Geeks Empire <Support@GeeksEmpire.net>
2019-05-17 20:59:13 -0800

    [Time] 05-17-2019 | 21:00
    [Description] Android Dynamic Feature | Install Features On Demand & In-App Update & Instant App
    
    [Phone][ChangeLog]
    ~ Google Assistant Action for Android
    ~ Instant App
    ~ App Bundle
    ~ Dynamic Feature
    ~ In-App Update | IMMEDIATE
    
    [Build][ChangeLog]
    ~

--- BaseModule/src/main/java/net/geeksempire/experimental/demonstration/AssistantAction.kt
+++ BaseModule/src/main/java/net/geeksempire/experimental/demonstration/AssistantAction.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.app.assist.AssistContent
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.appindexing.Action
 import com.google.firebase.appindexing.FirebaseUserActions
 import com.google.firebase.appindexing.builders.AssistActionBuilder
 import org.json.JSONObject
@@ -12,20 +13,21 @@ class AssistantAction : Activity() {
 
     private val actionTokenExtra = "actions.fulfillment.extra.ACTION_TOKEN"
 
+    var AppNameToLaunch: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //APP_ACTION
-        val actionIntent = intent
-        val appLinkAction = actionIntent.action
-        val appLinkData = actionIntent.data
+        val intent = intent
+        val actionIntent = intent.action
+        val actionData = intent.data
 
-        Toast.makeText(
-            applicationContext,
-            appLinkAction + "\n"
-                    + intent.getStringExtra("feature") + "\n"
-                    + appLinkData, Toast.LENGTH_LONG
-        ).show()
+        AppNameToLaunch =
+            actionData.toString().replace("https://geeksempire.net/DemoAssistantActions/AppToLaunch=?AppName=", "")
+
+        Toast.makeText(applicationContext, AppNameToLaunch, Toast.LENGTH_LONG).show()
+
+        notifyActionStatus(Action.Builder.STATUS_TYPE_COMPLETED)
     }
 
     override fun onProvideAssistContent(outContent: AssistContent) {
@@ -33,8 +35,8 @@ class AssistantAction : Activity() {
 
         // JSON-LD object based on Schema.org structured data
         outContent.structuredData = JSONObject()
-            .put("@type", "TYPE")
-            .put("name", "ACTION NAME")
+            .put("@type", "feature")
+            .put("name", AppNameToLaunch)
             .put("url", "https://geeksempire.net/DemoAssistantActions")
             .toString()
     }
