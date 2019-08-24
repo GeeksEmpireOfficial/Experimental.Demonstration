ae77ee045abd4efb6936925dcd74ef375ed853d1
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-08-23 16:39:24 -0800

    [Time] 23-Aug-2019 | 16:40
    [Description] ⚠ Demonstration Of Experimental Coding ⚠
    
    [Phone][ChangeLog]
    ~ Facebook Login & Retrieving Data:
    - Profile Picture
    ~ Braintree SandBox Environment
    ~ PayPal Test Environment
    ~ Google Pay Test Environment
    
    [Watch][ChangeLog]
    ~
    
    [Build][ChangeLog]
    ~ Libraries Updated

--- BaseModule/src/main/java/net/geeksempire/experimental/demonstration/Facebook/FacebookProcess.kt
+++ BaseModule/src/main/java/net/geeksempire/experimental/demonstration/Facebook/FacebookProcess.kt
@@ -1,13 +1,105 @@
 package net.geeksempire.experimental.demonstration.Facebook
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import com.facebook.login.widget.ProfilePictureView
 import net.geeksempire.experimental.demonstration.R
+import net.geekstools.floatshort.PRO.Util.Functions.FunctionsClassDebug
 
 class FacebookProcess : Activity() {
+
+    lateinit var callbackManager: CallbackManager
+
+    lateinit var profileFacebook: Profile
+
+    lateinit var profilePicture: ProfilePictureView
+
+    private val facebookPermissionsProfile = "public_profile"
+    private val facebookPermissionsEmail = "email"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.facebook_view)
+
+        val loginButton = findViewById<LoginButton>(R.id.loginFacebook) as LoginButton
+        profilePicture = findViewById<ProfilePictureView>(R.id.profilePicture) as ProfilePictureView
+
+        callbackManager = CallbackManager.Factory.create()
+
+        val accessToken = AccessToken.getCurrentAccessToken()
+        val isLoggedIn = accessToken != null && !accessToken.isExpired
+        if (isLoggedIn) {
+            profileFacebook = Profile.getCurrentProfile()
+            profilePicture.profileId = profileFacebook.id
+        }
+
+        loginButton.setPermissions(listOf(facebookPermissionsEmail, facebookPermissionsProfile))
+        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                FunctionsClassDebug.PrintDebug("*** ${result} ***")
+
+                if (isLoggedIn) {
+                    profileFacebook = Profile.getCurrentProfile()
+                    profilePicture.profileId = profileFacebook.id
+                }
+            }
+
+            override fun onCancel() {
+                FunctionsClassDebug.PrintDebug("*** Cancel ***")
+            }
+
+            override fun onError(error: FacebookException?) {
+                FunctionsClassDebug.PrintDebug("*** ${error} ***")
+
+            }
+        })
+
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    FunctionsClassDebug.PrintDebug("*** ${result} ***")
+
+                    if (isLoggedIn) {
+                        profileFacebook = Profile.getCurrentProfile()
+                        profilePicture.profileId = profileFacebook.id
+                    }
+                }
+
+                override fun onCancel() {
+                    FunctionsClassDebug.PrintDebug("*** Cancel ***")
+                }
+
+                override fun onError(error: FacebookException?) {
+                    FunctionsClassDebug.PrintDebug("*** ${error} ***")
+
+                }
+
+            })
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val accessToken = AccessToken.getCurrentAccessToken()
+        val isLoggedIn = accessToken != null && !accessToken.isExpired
+        if (isLoggedIn) {
+            profileFacebook = Profile.getCurrentProfile()
+            profilePicture.profileId = profileFacebook.id
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
--- BaseModule/src/main/java/net/geeksempire/experimental/demonstration/TestSelectionActivity.kt
+++ BaseModule/src/main/java/net/geeksempire/experimental/demonstration/TestSelectionActivity.kt
@@ -3,7 +3,9 @@ package net.geeksempire.experimental.demonstration
 import android.Manifest
 import android.app.ActivityOptions
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
 import android.view.View
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -18,6 +20,10 @@ import net.geeksempire.experimental.demonstration.Facebook.FacebookProcess
 import net.geeksempire.experimental.demonstration.GooglePayProcess.InitializeGooglePay
 import net.geeksempire.experimental.demonstration.PayPalProcess.InitializePayPal
 import net.geeksempire.experimental.demonstration.Utils.Functions.FunctionsClass
+import net.geekstools.floatshort.PRO.Util.Functions.FunctionsClassDebug
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 class TestSelectionActivity : BaseConfigurations() {
 
@@ -63,6 +69,27 @@ class TestSelectionActivity : BaseConfigurations() {
 
         facebookAPI.setOnClickListener {
             startActivity(Intent(applicationContext, FacebookProcess::class.java))
+        }
+
+        if (BuildConfig.DEBUG) {
+            try {
+                val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+                for (signature in info.signatures) {
+                    val messageDigest = MessageDigest.getInstance("SHA")
+                    messageDigest.update(signature.toByteArray())
+                    FunctionsClassDebug.PrintDebug(
+                        "KeyHash: ${Base64.encodeToString(
+                            messageDigest.digest(),
+                            Base64.DEFAULT
+                        )} "
+                    )
+                }
+            } catch (e: PackageManager.NameNotFoundException) {
+
+            } catch (e: NoSuchAlgorithmException) {
+
+            }
+
         }
 
         firebaseAuth = FirebaseAuth.getInstance()
