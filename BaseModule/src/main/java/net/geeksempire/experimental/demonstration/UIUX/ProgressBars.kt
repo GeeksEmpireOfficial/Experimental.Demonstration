8ae9f641a6f9f8b4f9506e1870189e7694f29a6e
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-10-12 06:24:19 -0800

    [Time] 12-Oct-2019 | 06:25
    [Description] ⚠ Demonstration Of Experimental Coding ⚠
    
    [Phone][ChangeLog]
    ~ Facebook Login (Link To Firebase) & Retrieving Data:
    - Profile Picture
    ~ Braintree SandBox Environment
    ~ PayPal Test Environment
    ~ Google Pay Test Environment
    
    [Watch][ChangeLog]
    ~
    
    [Build][ChangeLog]
    ~ Libraries Updated

---
+++ BaseModule/src/main/java/net/geeksempire/experimental/demonstration/UIUX/ProgressBars.kt
@@ -0,0 +1,13 @@
+package net.geeksempire.experimental.demonstration.UIUX
+
+import android.app.Activity
+import android.os.Bundle
+import net.geeksempire.experimental.demonstration.R
+
+class ProgressBars : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.progress_bars)
+    }
+}
