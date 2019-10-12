6ffa12e0edce64b72bef04b78c4874bfadf612b0
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-10-12 11:09:01 -0800

    [Time] 12-Oct-2019 | 11:10
    [Description] ⚠ Demonstration Of Experimental Coding ⚠
    
    [Phone][ChangeLog]
    ~ Facebook Login (Link To Firebase) & Retrieving Data:
    - Profile Picture
    ~ Braintree SandBox Environment
    ~ PayPal Test Environment
    ~ Google Pay Test Environment
    
    [Watch][ChangeLog]
    ~
    
    [Build][ChangeLog]
    ~ Libraries Updated

--- BaseModule/src/main/java/net/geeksempire/experimental/demonstration/UI/MaterialUI.kt
+++ BaseModule/src/main/java/net/geeksempire/experimental/demonstration/UI/MaterialUI.kt
@@ -2,15 +2,16 @@ package net.geeksempire.experimental.demonstration.UI
 
 import android.app.Activity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.progress_bars.*
+import kotlinx.android.synthetic.main.ui_material.*
 import net.geeksempire.experimental.demonstration.R
 
 class MaterialUI : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.progress_bars)
+        setContentView(R.layout.ui_material)
 
-        progressBar.progress = 77f
+        horizontalProgressView.progress = 77f
+        circleProgressView.progress = 55f
     }
 }
