c7232b6dbb32892bdfd38542cd8f48df66597adf
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-02-27 23:27:11 -0900

    [Phone][ChangeLog] 2. VectorDrawable Animation 1. Lottie Animation
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    ~ Libraries Updated

--- BaseModule/src/main/java/net/geeksempire/experimental/demonstration/UI/MaterialUI.kt
+++ BaseModule/src/main/java/net/geeksempire/experimental/demonstration/UI/MaterialUI.kt
@@ -1,6 +1,9 @@
 package net.geeksempire.experimental.demonstration.UI
 
 import android.app.Activity
+import android.graphics.drawable.Animatable2
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import kotlinx.android.synthetic.main.ui_material.*
 import net.geeksempire.experimental.demonstration.R
@@ -11,9 +14,13 @@ class MaterialUI : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ui_material)
 
-        horizontalProgressView.progress = 77f
-        circleProgressView.progress = 55f
+        val animatedVectorDrawable: AnimatedVectorDrawable = vectorControl.drawable as AnimatedVectorDrawable
+        animatedVectorDrawable.start()
 
-
+        animatedVectorDrawable?.registerAnimationCallback(object : Animatable2.AnimationCallback() {
+            override fun onAnimationEnd(drawable: Drawable?) {
+                animatedVectorDrawable.start()
+            }
+        })
     }
 }
