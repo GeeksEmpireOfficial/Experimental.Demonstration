a71263f96f384876189547cd29ddb55d529699b8
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-11-17 10:13:02 -0900

    [Phone][ChangeLog]
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    ~ Libraries Updated

---
+++ BaseModule/src/main/java/net/geeksempire/experimental/demonstration/PhysicsAnimation/DifferentMotions.kt
@@ -0,0 +1,12 @@
+package net.geeksempire.experimental.demonstration.PhysicsAnimation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DifferentMotions : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
