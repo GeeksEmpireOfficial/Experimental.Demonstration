f9648cae83a2bbdf9d59077ee78966f5112da121
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-05-10 02:59:11 -0800

    [Phone][ChangeLog]
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    ~ Dependencies Updated

--- BaseModule/src/main/java/net/geeksempire/experimental/demonstration/UI/MaterialUI.kt
+++ BaseModule/src/main/java/net/geeksempire/experimental/demonstration/UI/MaterialUI.kt
@@ -5,10 +5,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
-import android.graphics.drawable.Animatable
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.LayerDrawable
-import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.*
 import android.graphics.drawable.shapes.RoundRectShape
 import android.os.Bundle
 import android.widget.ImageView
@@ -32,39 +29,47 @@ class MaterialUI : Activity() {
 
     fun setupNegativeSpaceShape() {
 
+        val gradientDrawableBackground = GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, intArrayOf(Color.BLUE, Color.RED, Color.BLACK, Color.CYAN))
+
+        rootView.background = gradientDrawableBackground
+
         val negativeSpaceLayers = getDrawable(R.drawable.negative_space_shape) as LayerDrawable
 
         val backgroundShadowRadius = floatArrayOf(0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f)
 
-        backgroundShadowRadius[0] = (51).toFloat()//topLeftCorner
-        backgroundShadowRadius[1] = (51).toFloat()//topLeftCorner
+        backgroundShadowRadius[0] = (99).toFloat()//topLeftCorner
+        backgroundShadowRadius[1] = (99).toFloat()//topLeftCorner
 
-        backgroundShadowRadius[2] = (51).toFloat()//topRightCorner
-        backgroundShadowRadius[3] = (51).toFloat()//topRightCorner
+        backgroundShadowRadius[2] = (99).toFloat()//topRightCorner
+        backgroundShadowRadius[3] = (99).toFloat()//topRightCorner
 
-        backgroundShadowRadius[4] = (51).toFloat()//bottomRightCorner
-        backgroundShadowRadius[5] = (51).toFloat()//bottomRightCorner
+        backgroundShadowRadius[6] = (99).toFloat()//bottomLeftCorner
+        backgroundShadowRadius[7] = (99).toFloat()//bottomLeftCorner
 
-        backgroundShadowRadius[6] = (51).toFloat()//bottomLeftCorner
-        backgroundShadowRadius[7] = (51).toFloat()//bottomLeftCorner
+        backgroundShadowRadius[4] = (19).toFloat()//bottomRightCorner
+        backgroundShadowRadius[5] = (19).toFloat()//bottomRightCorner
 
         val shapeShadow: ShapeDrawable = ShapeDrawable(RoundRectShape(backgroundShadowRadius, null, null))
-        val shapeShadowPaint = shapeShadow.paint.apply {
+        shapeShadow.paint.apply {
             style = Paint.Style.FILL
             color = Color.TRANSPARENT
-            alpha = 155
+            isAntiAlias = true
 
             xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
         }
 
-//        val negativeShapeDrawable = negativeSpaceLayers.findDrawableByLayerId(R.id.clearLayer).toBitmap()
-
-
         negativeSpaceLayers.setDrawableByLayerId(R.id.clearLayer, shapeShadow)
 
-        negativeSpaceDesign.setLayerType(ImageView.LAYER_TYPE_HARDWARE, shapeShadowPaint)
+        negativeSpaceDesign.setLayerType(ImageView.LAYER_TYPE_SOFTWARE, shapeShadow.paint)
+        negativeSpaceDesign.setLayerType(ImageView.LAYER_TYPE_HARDWARE, null)
 
         negativeSpaceDesign.setImageDrawable(negativeSpaceLayers)
+
+        negativeSpaceDesign.setOnClickListener {
+
+
+
+        }
 
     }
 }
