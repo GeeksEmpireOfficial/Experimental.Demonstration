dcf4fe2b635a69693716bf32e6b4d5b07436caf4
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-05-25 14:22:21 -0800

    [Phone][ChangeLog]
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    ~ Dependencies Updated

--- BaseModule/src/main/java/net/geeksempire/experimental/demonstration/UI/MaterialUI.kt
+++ BaseModule/src/main/java/net/geeksempire/experimental/demonstration/UI/MaterialUI.kt
@@ -1,15 +1,17 @@
+
 package net.geeksempire.experimental.demonstration.UI
 
 import android.app.Activity
-import android.graphics.drawable.Animatable
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.LayerDrawable
-import android.graphics.drawable.ShapeDrawable
+import android.graphics.*
+import android.graphics.drawable.*
 import android.graphics.drawable.shapes.RoundRectShape
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.drawable.toDrawable
 import kotlinx.android.synthetic.main.ui_material.*
 import net.geeksempire.experimental.demonstration.R
+
 
 class MaterialUI : Activity() {
 
@@ -17,18 +19,27 @@ class MaterialUI : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ui_material)
 
-        val animatable = getDrawable(R.drawable.animated_geeksempire) as Animatable
-        animatable.start()
+        if (vectorControlOne.isShown) {
 
-        vectorControlOne.setImageDrawable(animatable as Drawable)
+            val animatable = getDrawable(R.drawable.animated_geeksempire) as Animatable
+            animatable.start()
 
-//        val gradientDrawableBackground = GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, intArrayOf(Color.BLUE, Color.RED, Color.BLACK, Color.CYAN))
-//
-//        rootView.background = gradientDrawableBackground
+            vectorControlOne.setImageDrawable(animatable as Drawable)
+
+        }
+
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+
+        rootView.setBackgroundColor(Color.TRANSPARENT)
+
+        window.setBackgroundDrawable(GradientDrawable(GradientDrawable.Orientation.TR_BL,
+            intArrayOf(Color.BLUE, Color.YELLOW, Color.GREEN, Color.CYAN, Color.RED, Color.MAGENTA)))
 
         with(negativeSpaceDesign) {
 
-            setupNegativeSpaceShape()
+//            setupNegativeSpaceShape()
+            setNegativeSpaceVectorDrawable()
 
         }
 
@@ -42,37 +53,35 @@ class MaterialUI : Activity() {
 
     fun setupNegativeSpaceShape() {
 
-        val negativeSpaceLayers = getDrawable(R.drawable.layer_shodow) as LayerDrawable
+        val negativeSpaceLayers = getDrawable(R.drawable.negative_space_shape) as LayerDrawable
 
-//        val backgroundShadowRadius = floatArrayOf(0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f)
-//
-//        backgroundShadowRadius[0] = (99).toFloat()//topLeftCorner
-//        backgroundShadowRadius[1] = (99).toFloat()//topLeftCorner
-//
-//        backgroundShadowRadius[2] = (99).toFloat()//topRightCorner
-//        backgroundShadowRadius[3] = (99).toFloat()//topRightCorner
-//
-//        backgroundShadowRadius[6] = (99).toFloat()//bottomLeftCorner
-//        backgroundShadowRadius[7] = (99).toFloat()//bottomLeftCorner
-//
-//        backgroundShadowRadius[4] = (19).toFloat()//bottomRightCorner
-//        backgroundShadowRadius[5] = (19).toFloat()//bottomRightCorner
-//
-//        val shapeShadow: ShapeDrawable = ShapeDrawable(RoundRectShape(backgroundShadowRadius, null, null))
-//        shapeShadow.paint.apply {
-//            style = Paint.Style.FILL
-//            color = Color.TRANSPARENT
-//            isAntiAlias = true
-//
-//            xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
-//        }
-//
-//        negativeSpaceLayers.setDrawableByLayerId(R.id.clearLayer, shapeShadow)
+        val backgroundShadowRadius = floatArrayOf(0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f)
 
-        setShadow(negativeSpaceLayers)
+        backgroundShadowRadius[0] = (99).toFloat()//topLeftCorner
+        backgroundShadowRadius[1] = (99).toFloat()//topLeftCorner
 
-//        negativeSpaceDesign.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, shapeShadow.paint)
-//        negativeSpaceDesign.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
+        backgroundShadowRadius[2] = (99).toFloat()//topRightCorner
+        backgroundShadowRadius[3] = (99).toFloat()//topRightCorner
+
+        backgroundShadowRadius[6] = (99).toFloat()//bottomLeftCorner
+        backgroundShadowRadius[7] = (99).toFloat()//bottomLeftCorner
+
+        backgroundShadowRadius[4] = (23).toFloat()//bottomRightCorner
+        backgroundShadowRadius[5] = (23).toFloat()//bottomRightCorner
+
+        val shapeShadow: ShapeDrawable = ShapeDrawable(RoundRectShape(backgroundShadowRadius, null, null))
+        shapeShadow.paint.apply {
+            style = Paint.Style.FILL
+            color = Color.TRANSPARENT
+            isAntiAlias = true
+
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        }
+
+        negativeSpaceLayers.setDrawableByLayerId(R.id.clearLayer, shapeShadow)
+
+        negativeSpaceDesign.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, shapeShadow.paint)
+        negativeSpaceDesign.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
 
         negativeSpaceDesign.setImageDrawable(negativeSpaceLayers)
 
@@ -82,17 +91,17 @@ class MaterialUI : Activity() {
 
         val backgroundShadowRadius = floatArrayOf(0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f)
 
-        backgroundShadowRadius[0] = (399).toFloat()//topLeftCorner
-        backgroundShadowRadius[1] = (399).toFloat()//topLeftCorner
+        backgroundShadowRadius[0] = (19).toFloat()//topLeftCorner
+        backgroundShadowRadius[1] = (19).toFloat()//topLeftCorner
 
-        backgroundShadowRadius[2] = (399).toFloat()//topRightCorner
-        backgroundShadowRadius[3] = (399).toFloat()//topRightCorner
+        backgroundShadowRadius[2] = (19).toFloat()//topRightCorner
+        backgroundShadowRadius[3] = (19).toFloat()//topRightCorner
 
-        backgroundShadowRadius[6] = (399).toFloat()//bottomLeftCorner
-        backgroundShadowRadius[7] = (399).toFloat()//bottomLeftCorner
+        backgroundShadowRadius[6] = (19).toFloat()//bottomLeftCorner
+        backgroundShadowRadius[7] = (19).toFloat()//bottomLeftCorner
 
-        backgroundShadowRadius[4] = (399).toFloat()//bottomRightCorner
-        backgroundShadowRadius[5] = (399).toFloat()//bottomRightCorner
+        backgroundShadowRadius[4] = (19).toFloat()//bottomRightCorner
+        backgroundShadowRadius[5] = (19).toFloat()//bottomRightCorner
 
         val shapeShadow: ShapeDrawable = ShapeDrawable(RoundRectShape(backgroundShadowRadius, null, null))
         shapeShadow.paint.apply {
@@ -101,10 +110,46 @@ class MaterialUI : Activity() {
             setShadowLayer(29f, 0f, 0f, getColor(R.color.dark))
         }
 
-        negativeSpaceLayers.setDrawableByLayerId(R.id.circleLayer, shapeShadow)
+        negativeSpaceLayers.setDrawableByLayerId(R.id.backgroundLayer, shapeShadow)
 
         negativeSpaceDesign.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, shapeShadow.paint)
         negativeSpaceDesign.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
+
+    }
+
+    fun setNegativeSpaceVectorDrawable() {
+
+        val negativeSpaceLayers = getDrawable(R.drawable.negative_space_shape) as LayerDrawable
+        val clearLayer = negativeSpaceLayers.findDrawableByLayerId(R.id.clearLayer)
+
+        val vectorShape = getDrawable(R.drawable.android) as VectorDrawable
+
+        val vectorBitmap = vectorShape.toBitmap()
+
+        val canvas = Canvas(vectorBitmap)
+
+        negativeSpaceLayers.setBounds(0, 0, canvas.width, canvas.height)
+
+        val paint = Paint().apply {
+            style = Paint.Style.FILL
+            color = Color.TRANSPARENT
+            isAntiAlias = true
+
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        }
+
+        canvas.drawBitmap(vectorBitmap, 0f, 0f, null)
+
+        clearLayer.draw(canvas)
+
+        negativeSpaceLayers.setDrawableByLayerId(R.id.clearLayer, vectorBitmap.toDrawable(resources))
+        negativeSpaceLayers.setDrawableByLayerId(R.id.clearLayer, /*vectorBitmap.toDrawable(resources)*/clearLayer)
+
+        negativeSpaceDesign.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, paint)
+        negativeSpaceDesign.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
+
+        negativeSpaceDesign.setImageDrawable(negativeSpaceLayers)
+//        negativeSpaceDesign.setImageBitmap(vectorBitmap)
 
     }
 
